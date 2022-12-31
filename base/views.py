ea4e3e68fa44f2101fbd709ed395b03be40e5563
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-31 09:44:38 +0300

    Backend: configure urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def getRoutes(request):
+    routes = [
+        "api/products/",
+        "api/products/id/",
+        "api/reviews/",
+        "api/orders/",
+        "api/orders/id/",
+    ]
+    return HttpResponse(routes)
