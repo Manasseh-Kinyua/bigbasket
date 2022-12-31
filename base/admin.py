71cb9798b6282ecc421f1cef1d5e5a6226d1f085
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-31 11:05:00 +0300

    Backend: register models with admin site

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Brand, Color, Category, Product, Order, Review, OrderItem, ShippingAddress
 
 # Register your models here.
+
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Review)
+admin.site.register(ShippingAddress)
