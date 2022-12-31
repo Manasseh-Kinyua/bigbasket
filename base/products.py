9b5ebd2f6c38c27fea14cba5bd76a4904cf2858a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-31 12:07:16 +0300

    Frontend: generate react files

---
+++ base/products.py
@@ -0,0 +1,82 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'Sony PlayStation 5 PS5 Digital Edition Console (US Plug)',
+    'image': '/images/airpods.jpg',
+    'description':
+      'With a retail price of $400 ($100 less than the Blu-ray Edition), this PlayStation PS5 (US Plug) Digital Edition Console comes without a disc drive and features a sleek white design with a matching DualSense controller.',
+    'brand': 'Sony',
+    'category': 'Gaming',
+    'price': 399.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Apple iPhone 14 Pro Max 128GB Space (eSIM)',
+    'image': '/images/phone.jpg',
+    'description':
+      'iPhone 14 Pro Max. Capture incredible detail with a 48MP Main camera. Experience iPhone in a whole new way with Dynamic Island and Always-On display. And get peace of mind with groundbreaking safety features.',
+    'brand': 'Apple',
+    'category': 'Electronics',
+    'price': 599.99,
+    'countInStock': 0,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Charger Station for PS5 Controller, Dual Charging Dock Stand',
+    'image': '/images/camera.jpg',
+    'description':
+      'Dual PS5 charging dock compatible to simultaneously store and charge two controllers in a secure manner. Power outages are not a concern. Two controllers may be charged simultaneously by the controller of the PS5 charger, and the charging duration is only 4 hours.',
+    'brand': 'Cannon',
+    'category': 'Gaming',
+    'price': 29.99,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'Sony Playstation 4 Pro White Version',
+    'image': '/images/playstation.jpg',
+    'description':
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    'brand': 'Sony',
+    'category': 'Electronics',
+    'price': 399.99,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Logitech G-Series Gaming Mouse',
+    'image': '/images/mouse.jpg',
+    'description':
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    'brand': 'Logitech',
+    'category': 'Electronics',
+    'price': 49.99,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Amazon Echo Dot 3rd Generation',
+    'image': '/images/alexa.jpg',
+    'description':
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    'brand': 'Amazon',
+    'category': 'Electronics',
+    'price': 29.99,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+]
+
+
