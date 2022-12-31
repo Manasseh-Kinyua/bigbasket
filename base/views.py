76a61d79e6d04ce19da3400547397dce03d92ccf
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-31 13:22:07 +0300

    Backend: create get-products view and url

--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -15,5 +17,8 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+@api_view(['GET'])
 def getProducts(request):
-    return Response('')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
