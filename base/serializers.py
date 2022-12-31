a17bbb480d3701ea4eef437b925fd88d62944a50
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-31 13:17:17 +0300

    Backend: serialize the product model data

---
+++ base/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Product
+from django.contrib.auth.models import User
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
