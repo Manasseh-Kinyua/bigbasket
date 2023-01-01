e663de124379c89595d54c41b7efe73be6e971f2
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-01 11:47:39 +0300

    Frontend: complete color setting

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Navbar,Nav, Image } from 'react-bootstrap'
+import { Container, Navbar, Nav, Row, Image } from 'react-bootstrap'
 
 function Header() {
   return (
@@ -7,13 +7,14 @@ function Header() {
       <Navbar style={{minHeight:'10vh'}} className='bg'  variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     {/* <LinkContainer to='/'> */}
-                        <Navbar.Brand><Image style={{width:'8%', marginRight:'.6rem'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV4rK81-ngDpTadiTjXawJXtwmxEY7ND-YhQ&usqp=CAU' /><strong>BigBasket</strong></Navbar.Brand>
+                        <Navbar.Brand><Image style={{width:'8%', marginRight:'.6rem'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1ML0x0ClmUtqfkoMKDWwqNPYUZpSb_0yh0D1hsvcBYavfJ1YhWxYZCP_GlzAO0bl9JHE&usqp=CAU' /><strong>BigBasket</strong></Navbar.Brand>
                     {/* </LinkContainer> */}
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-
+                          <Nav.Link href='cart'>Cart</Nav.Link>
+                          <Nav.Link href='login'>Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
