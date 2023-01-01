c3d66014284f05f6e6f3b45b4f0e28fb9be759b6
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-01 10:12:46 +0300

    Backend: single product view and url

--- base/views.py
+++ base/views.py
@@ -22,3 +22,9 @@ def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getSingleProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
