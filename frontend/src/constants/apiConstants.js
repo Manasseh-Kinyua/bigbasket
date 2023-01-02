d276bbe993d484ded1e05657eda04578e921352e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-02 10:13:15 +0300

    Frontend: create product list reducer

---
+++ frontend/src/constants/apiConstants.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://127.0.0.1:8000/api/"
+
+export const GET_PRODUCTS_ENDPOINT = `${BASE_URL}products/`
+export const GET_SINGLE_PRODUCT_ENDPOINT = `${BASE_URL}products/`
