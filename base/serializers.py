38f23874b0b7fc73419eaa6876231c704f948672
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-03 12:02:52 +0300

    Backend: nest brand, category and color serializers into product serializer

--- base/serializers.py
+++ base/serializers.py
@@ -18,6 +18,25 @@ class ColorSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField(read_only=True)
+    brand = serializers.SerializerMethodField(read_only=True)
+    color = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_category(self, obj):
+        category = obj.category
+        serializer = CategorySerializer(category, many=False)
+        return serializer.data
+
+    def get_brand(self, obj):
+        brand = obj.brand
+        serializer = BrandSerializer(brand, many=False)
+        return serializer.data
+
+    def get_color(self, obj):
+        color = obj.color
+        serializer = BrandSerializer(color, many=False)
+        return serializer.data
