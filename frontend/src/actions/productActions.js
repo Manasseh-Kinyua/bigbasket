c59eb3a4a9dececa970a248fe849517c397d1774
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-03 11:15:09 +0300

    Frontend: product details action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,8 +2,12 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
-import { GET_PRODUCTS_ENDPOINT } from "../constants/apiConstants";
+import { GET_PRODUCTS_ENDPOINT, GET_SINGLE_PRODUCT_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
@@ -19,6 +23,26 @@ export const listProducts = () => async (dispatch) => {
     catch(error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
+
+        const {data} = await axios.get(`${GET_SINGLE_PRODUCT_ENDPOINT}${id}`)
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    }
+    catch(error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
