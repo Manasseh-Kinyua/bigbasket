485b8e0e2e4604059642da85f0f5666eac04c927
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-03 11:56:09 +0300

    Backend: serialize brand, category and color

--- base/serializers.py
+++ base/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Brand, Color, Category
 from django.contrib.auth.models import User
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = "__all__"
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = "__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
