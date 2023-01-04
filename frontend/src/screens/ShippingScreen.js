7b958290514e377fbe657e4194bcb1cb68c9bab6
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-04 15:54:51 +0300

    Frontend: dispatch save shipping address action

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,24 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Container from '@mui/material/Container';
 import { Row, Col, Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { saveShippingAddress } from '../actions/cartActions';
 
 function ShippingScreen() {
 
-    const [address, setAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [postalCode, setPostalCode] = useState('')
-    const [country, setCountry] = useState('')
+    const dispatch = useDispatch()
 
-    useEffect(() => {
+    const cart = useSelector(state => state.cart)
+    const {shippingAddress} = cart
+    console.log(shippingAddress)
 
-    })
+    const [address, setAddress] = useState(shippingAddress.address)
+    const [city, setCity] = useState(shippingAddress.city)
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+    const [country, setCountry] = useState(shippingAddress.country)
+
+    const submitShippingAddressHandler = (e) => {
+      e.preventDefault()
+
+      dispatch(saveShippingAddress({
+        address, city, postalCode, country
+      }))
+    }
 
   return (
     <div>
       <Container maxWidth='xl'>
         <Row>
             <Col className='mx-auto my-4' md={6}>
-                <Form className='p-4'>
+                <Form className='p-4' onSubmit={submitShippingAddressHandler}>
                     <h1 className='text-light'>Shipping</h1>
                     <Form.Group className='my-4' controlId='address'>
                         <Form.Label className='text-light'>Address</Form.Label>
@@ -57,6 +69,7 @@ function ShippingScreen() {
                             style={{backgroundColor:'rgb(17, 17, 17)'}}></Form.Control>
                     </Form.Group>
                     <Button
+                        type='submit'
                         style={{backgroundColor:'red', width:'100%'}}>Proceed</Button>
                 </Form>
             </Col>
