380ce2c66dc3037ae4c659f2271ffdf5a2295182
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-04 12:19:22 +0300

    Frontend: create cart list in cart page

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import Container from '@mui/material/Container';
-import { Row, Col } from 'react-bootstrap'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Row, Col, ListGroup, Image, Button, Form } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation, useParams, Link } from 'react-router-dom'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { addToCart } from '../actions/cartActions'
@@ -23,12 +24,49 @@ function CartScreen() {
     console.log('CARTITEMS.........', cartItems)
 
     useEffect(() => {
-      dispatch(addToCart(params.id, quantity))
+      if(params.id) {
+        dispatch(addToCart(params.id, quantity))
+      }
     }, [dispatch, params.id, quantity])
   return (
     <div>
       <Container maxWidth='xl'>
+        <Row className='mt-4'>
+          <Link className='text-light my-3' to='/products'>Back to Products</Link>
+          <Col md={8}>
+            <h3 className='text-light'>MY CART</h3>
 
+            {cartItems.length < 1 ? (
+              <Message variant='info'>Ooops!!! Seems like you have not started shopping. Shop in order to see your Cart</Message>
+            ) : (
+              <ListGroup>
+                {cartItems.map(item => (
+                  <div key={item.product} className='flexed p-3 m-2 bg'>
+                    <Image src={item.image} alt='item.name' style={{width:'10%'}} className='mx-1' />
+                    <Link to={`/product/${item.product}`} className='text-light mx-1'>{item.name}</Link>
+                    <strong className='orange-txt mx-1'>${item.price}</strong>
+                    <Form>
+                      <Form.Group>
+                        <Form.Control
+                          as='select'
+                          value={item.quantity}
+                          onChange={(e) => dispatch(addToCart(Number(item.product), Number(e.target.value)))}
+                          style={{backgroundColor:'black', color:'white'}}>
+                            {
+                              [...Array(item.countInStock).keys()].map(x => (
+                                <option value={x+1} key={x+1}>{x+1}</option>
+                                ))
+                             }
+                        </Form.Control>
+                      </Form.Group>
+                    </Form>
+                    <Button style={{backgroundColor:'rgb(17, 17, 17)'}}><DeleteIcon /></Button>
+                  </div>
+                ))}
+              </ListGroup>
+            )}
+          </Col>
+        </Row>
       </Container>
     </div>
   )
