60b8fe59b83854d11553126f093fcf44a21d7db3
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-05 10:53:42 +0300

    Backend: register user view and url

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='user-register'),
     path('users/profile/', views.getUserProfile, name='user-profile'),
     path('users/', views.getUsers, name='users'),
     path('', views.getRoutes, name='routes'),
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -35,6 +36,20 @@ def getRoutes(request):
         "api/orders/id/",
     ]
     return Response(routes)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    user = User.objects.create(
+        first_name = data['name'],
+        username = data['email'],
+        email = data['email'],
+        password = make_password(data['password'])
+    )
+    serializer = UserSerializerWithToken(user, many=False)
+
+    return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
