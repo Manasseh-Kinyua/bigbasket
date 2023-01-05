fc240c31f6afd3d775c88c50addbe19071cdb97a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-05 10:58:12 +0300

    Backend: use status codes for registration with an existing email

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -41,15 +42,19 @@ def getRoutes(request):
 def registerUser(request):
     data = request.data
 
-    user = User.objects.create(
-        first_name = data['name'],
-        username = data['email'],
-        email = data['email'],
-        password = make_password(data['password'])
-    )
-    serializer = UserSerializerWithToken(user, many=False)
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
