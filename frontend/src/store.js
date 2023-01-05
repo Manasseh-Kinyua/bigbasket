850ea6ae714d564f14ea2f824195588f901aca72
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-05 11:46:42 +0300

    Frontend: user login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,15 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
 
     cart: cartReducer,
+
+    userLogin: userLoginReducer,
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ?
