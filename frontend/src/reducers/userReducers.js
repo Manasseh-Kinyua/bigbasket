24de06369148072597b8999d9717bea7db83828a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-05 15:51:35 +0300

    Frontend: user details reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -7,6 +7,10 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -49,6 +53,30 @@ export const userRegisterReducer = (state = {}, action) => {
             }
 
         case USER_REGISTER_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = {}, action) => {
+    switch(action.type) {
+        case USER_DETAILS_REQUEST:
+            return {
+                loading: true
+            }
+
+        case USER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                user: action.payload
+            }
+
+        case USER_DETAILS_FAIL:
             return {
                 loading: false,
                 error: action.payload
