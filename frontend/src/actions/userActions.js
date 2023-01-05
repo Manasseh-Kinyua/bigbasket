0c013421ec2aad40d000adf3236107be0e2e1cca
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-05 16:23:04 +0300

    Frontend: edit profile action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -12,8 +12,12 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+
+    USER_EDIT_PROFILE_REQUEST,
+    USER_EDIT_PROFILE_SUCCESS,
+    USER_EDIT_PROFILE_FAIL,
 } from "../constants/userConstants";
-import { USER_LOGIN_ENDPOINT, USER_REGISTER_ENDPOINT, GET_USER_DETAILS_ENDPOINT } from "../constants/apiConstants";
+import { USER_LOGIN_ENDPOINT, USER_REGISTER_ENDPOINT, GET_USER_DETAILS_ENDPOINT, EDIT_USER_PROFILE_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
@@ -118,6 +122,47 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     } catch(error) {
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const editUserProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({type: USER_EDIT_PROFILE_REQUEST})
+
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get(
+            EDIT_USER_PROFILE_ENDPOINT,
+            user,
+            config
+        )
+
+        dispatch({
+            type: USER_EDIT_PROFILE_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+    } catch(error) {
+        dispatch({
+            type: USER_EDIT_PROFILE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
