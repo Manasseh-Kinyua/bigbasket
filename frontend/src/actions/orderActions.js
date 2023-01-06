053b5ab63968ae5c4fa8d9bf11751c5159fdab31
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-06 15:11:26 +0300

    Frontend: clear cart after creating order

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -4,6 +4,7 @@ import {
     CREATE_ORDER_FAIL,
 } from "../constants/orderConstants";
 import { CREATE_ORDER_ENDPOINT } from "../constants/apiConstants";
+import { CART_CLEAR } from "../constants/cartConstants";
 import axios from 'axios'
 
 export const createOrderAction = (order) => async (dispatch, getState) => {
@@ -31,6 +32,11 @@ export const createOrderAction = (order) => async (dispatch, getState) => {
             type: CREATE_ORDER_SUCCESS,
             payload: data
         })
+
+        dispatch({
+            type: CART_CLEAR,
+        })
+        localStorage.removeItem('cartItems')
     } catch(error) {
         dispatch({
             type: CREATE_ORDER_FAIL,
