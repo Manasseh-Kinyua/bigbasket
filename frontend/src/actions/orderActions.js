dc2329b358356b9edc77141d1e0db40260f84f9b
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-07 10:14:59 +0300

    Frontend: get user orders action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -11,8 +11,12 @@ import {
     GET_ORDER_DETAILS_REQUEST,
     GET_ORDER_DETAILS_SUCCESS,
     GET_ORDER_DETAILS_FAIL,
+
+    GET_USER_ORDERS_REQUEST,
+    GET_USER_ORDERS_SUCCESS,
+    GET_USER_ORDERS_FAIL,
 } from "../constants/orderConstants";
-import { CREATE_ORDER_ENDPOINT, GET_SINGLE_ORDER_ENDPOINT, PAY_ORDER_ENDPOINT } from "../constants/apiConstants";
+import { CREATE_ORDER_ENDPOINT, GET_SINGLE_ORDER_ENDPOINT, GET_USER_ORDERS_ENDPOINT, PAY_ORDER_ENDPOINT } from "../constants/apiConstants";
 import { CART_CLEAR } from "../constants/cartConstants";
 import axios from 'axios'
 
@@ -117,6 +121,40 @@ export const payOrder = (id) => async (dispatch, getState) => {
     } catch(error) {
         dispatch({
             type: PAY_ORDER_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const getUserOrders = () => async (dispatch, getState) => {
+    try {
+        dispatch({type: GET_USER_ORDERS_REQUEST})
+
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get(
+            GET_USER_ORDERS_ENDPOINT,
+            config
+        )
+
+        dispatch({
+            type: GET_USER_ORDERS_SUCCESS,
+            payload: data
+        })
+    } catch(error) {
+        dispatch({
+            type: GET_USER_ORDERS_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
