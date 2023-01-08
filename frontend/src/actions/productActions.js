97b3eae60f0de7db19f162e676a7b29002f13fa8
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-08 12:49:56 +0300

    Frontend: create product action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,8 +10,12 @@ import {
     PRODUCT_DELETE_REQUEST,
     PRODUCT_DELETE_SUCCESS,
     PRODUCT_DELETE_FAIL,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL,
 } from "../constants/productConstants";
-import { DELETE_PRODUCT_ENDPOINT, GET_PRODUCTS_ENDPOINT, GET_SINGLE_PRODUCT_ENDPOINT } from "../constants/apiConstants";
+import { CREATE_PRODUCT_ENDPOINT, DELETE_PRODUCT_ENDPOINT, GET_PRODUCTS_ENDPOINT, GET_SINGLE_PRODUCT_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
@@ -82,6 +86,42 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     } catch(error) {
         dispatch({
             type: PRODUCT_DELETE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const createProduct = (product) => async (dispatch, getState) => {
+    try {
+        dispatch({type: PRODUCT_CREATE_REQUEST})
+
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.post(
+            CREATE_PRODUCT_ENDPOINT,
+            product,
+            config
+        )
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+        })
+
+    } catch(error) {
+        dispatch({
+            type: PRODUCT_CREATE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
