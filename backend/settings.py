ad289ff5b20322475c5224384a21dab95ff0bee3
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-12 15:59:07 +0300

    Deployment: collectstatic

--- backend/settings.py
+++ backend/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR / 'frontend/build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
