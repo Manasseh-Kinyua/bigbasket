63e7cf0c0440e7a11f0b9482b9723101b1697d7a
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-01-12 15:08:40 +0300

    Deployment: manage modes

--- backend/settings.py
+++ backend/settings.py
@@ -137,10 +137,6 @@ import dj_database_url
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
-
 if os.getenv('MODE')=="dev":
    DATABASES = {
     'default': {
